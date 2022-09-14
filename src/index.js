3cea2c9e43ba894ec189d168b539c0e0fb1ceebc
Broomkashi <abhishektiwari20072001@gmail.com>
2022-09-14 13:03:47 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+//const arr = ['Abhishek', 'Baba','ballia']
+ReactDOM.render(
+    <>
+    <h1>Mahadev</h1>
+    <p>List of 5 Best Series
+    </p> 
+    <h2> 1.Dark</h2>
+    <h2> 2.Extra curricular </h2>
+    <h2> 3.My Holo Love</h2>
+    <h2> 4.My first-2 love</h2>
+    <h2> 5. Mr Robot</h2>
+
+    </>,
+
+  
+    document.getElementById("root")
+);
